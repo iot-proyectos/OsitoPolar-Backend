9f6ca87e1b10b5e39075273de639463e878ccbe4
diegoalonzomora <u20231c069@upc.edu.pe>
2026-07-10 14:10:15 -0500

    chore

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -2,6 +2,6 @@ import Stripe from 'stripe';
 import { env } from './env';
 
 export const stripe = new Stripe(env.stripe.secretKey, {
-  apiVersion: '2025-02-24.acacia',
+  apiVersion: '2026-06-24.dahlia',
   typescript: true,
 });
